8fb2af5c62511b02cb6336d28795b290454eb0e2
KMx404 <mohamedelfaleh5@gmail.com>
2020-07-05 19:39:31 +0100

    code edited

--- Quse.py
+++ Quse.py
@@ -4,9 +4,18 @@
 import os, sys
 import subprocess
 import random
-from cryptography.fernet import Fernet 
+import pyaes, pbkdf2, binascii, secrets
+import pyAesCrypt
+from cryptography.fernet import Fernet
+from os import stat, remove 
+bufferSize = 64 * 1024 
+password = "kmx404isthebest"
+pyAesCrypt.encryptFile("data.txt", "data.txt.aes", password, bufferSize) 
+pyAesCrypt.decryptFile("data.txt", "data.txt.aes", password, bufferSize) 
+
+# hash related
 from Crypto.Hash import SHA256
-from Crypto.Cipher import AES 
+from Crypto.Cipher import AES
 
 # banner
 banner = """
@@ -35,10 +44,30 @@ def st_dsp():
     print("2- RSA")
     print("3- TripleDES")
  
-
-def dAES(): 
-    obj = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV256')
-
+def aes_q() :
+    an = input("You want to decrypt, encrypt with AES?\n") 
+   
+# AES decrypting 
+class AES: 
+    def __init__(self): 
+        pass 
+    
+    # AES encrypt 
+    def eAES(): 
+        obj = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV256')
+        path = input("type the file path: ") 
+        dfile = obj.encrypt(path) 
+        with open(str(path), "rb") as fIn: 
+            with open(str(path)+".aes", "wb") as fOut: 
+                pyAesCrypt.encryptStream(fIn, fOut, password, bufferSize) 
+    # AES decrypt
+    def dAES(): 
+        with open(str(path)+".aes", "rb") as fIn: 
+            try: 
+                with open ("dataout.txt", "wb")  as fOut: 
+                    pyAesCrypt.decryptStream(fIn, fOut, password, buffersize, encFileSize) 
+            except ValueError: 
+                remove("dataout.txt") 
 
 # root check 
 def prompt_sudo():
@@ -48,11 +77,20 @@ def prompt_sudo():
         ret = subprocess.check_call("sudo -v -p '%s'" % msg, shell=True)
     return ret
 
-def main(): 
+if __name__=='__main__':
     print(banner)
     prompt_sudo() 
     if prompt_sudo() != 0: 
         print("You need SUDO for this!")
         exit() 
     st_dsp() 
-if __name__=='__main__':main()
+    answer = input("$  ") 
+    if int(answer) == 1: 
+        aes_q()
+        if int(aes_a) == 1: 
+            AES.eAES() 
+        else: 
+            AES.dAES() 
+    elif int(answer) == 2: 
+        # This one to be edited.  
+        pass
