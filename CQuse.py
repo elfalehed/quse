98c2ae8e94262902f5d51697c4e7faf5c05784f8
KMx404 <mohamedelfaleh5@gmail.com>
2020-07-08 18:35:41 +0100

    CQuse added && Cleaner code

---
+++ CQuse.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3 
+
+"""
+This is the current Version of Quse, It made for the current use! The other one works as well 
+but, this script made for beeter demponstration when we talk about the source code.
+"""
+
+
+
+
+
